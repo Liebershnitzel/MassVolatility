2dfaf30607183d6be3fd5e8bc278b97bebef47dd
Ben Lieberman <49920398+Liebershnitzel@users.noreply.github.com>
2024-08-07 14:58:10 -0400

    Add files via upload

--- massvolatility.py
+++ massvolatility.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python3
 import os
 import subprocess
@@ -8,7 +9,7 @@ import shutil
 import argparse
 #===============================================================#
 #Created by Ben Lieberman
-#Revision 2.0
+#Revision 3.0
 #===============================================================#
 
 
@@ -18,6 +19,7 @@ directories = argparse.ArgumentParser(description="Automatically parses with sel
 directories.add_argument("-v", "--volatility-dir",help="volatility3 directory location",required=True)
 directories.add_argument("-o","--output-dir",help="output directory location",required=True)
 directories.add_argument("-m","--memory-dumps",help="memory dumps location",required=True)
+directories.add_argument("--output-mode", help="Specify output mode (json or text)", choices=['json', 'text'], default='text')
 args = directories.parse_args()
 print(args.output_dir)
 print(args.volatility_dir)
@@ -43,7 +45,7 @@ dir_name = "Outputs"
 output_directory = os.path.join(args.output_dir, dir_name)
 os.makedirs(output_directory, exist_ok=True)
 
-       
+
 #==================================================================================================================#
 
 #===============================================================#
@@ -61,7 +63,7 @@ def get_os_profile(memory_dump):
     else:
         symbol_path = None
         print(f"Could not find symbol path in output for memory dump: {memory_dump}")
-    
+
     return symbol_path
 #==================================================================================================================#
 
@@ -97,22 +99,49 @@ elif int(chose) == 2:
 
 #Running Volatility Based on Defined Plugins
 
-def run_volatility_plugin(memory_dump, plugins, symbol_path):
+def run_volatility_plugin(memory_dump, plugins, symbol_path, output_mode):
     symbol_dir = os.path.join("/tmp", os.path.basename(memory_dump) + "_symbols")
-    os.makedirs(symbol_dir, exist_ok=True)    
+    os.makedirs(symbol_dir, exist_ok=True)
     memory_dump_name = os.path.basename(memory_dump).split('.')[0]
-    output_file_path = os.path.join(output_directory, f"{memory_dump_name}_{plugins}.txt")
     shutil.copy(symbol_path, symbol_dir)
     symbol_file_in_new_dir = os.path.join(symbol_dir, os.path.basename(symbol_path))
-    command = f"python3 {args.volatility_dir}vol.py -f {memory_dump} -s {symbol_dir} {plugins}"
-    with open(output_file_path, 'w') as output_file:
-        subprocess.run(command, shell=True, stdout=output_file)
-    
-    with open(output_file_path, 'r') as output_file:
-        print(output_file.read())
+
+    if output_mode == "json":
+        print(output_mode)
+        output_file_path = os.path.join(output_directory, f"{memory_dump_name}_{plugins}.json")
+        print("Currently Running", plugins)
+        command = f"python3 {args.volatility_dir}vol.py -f {memory_dump} -r json -s {symbol_dir} {plugins}"
+        try:
+            with open(output_file_path, 'w') as output_file:
+                subprocess.run(command, shell=True, stdout=output_file)
+            with open(output_file_path, 'r') as file:
+                data = json.load(file)
+            for item in data:
+                item['Host'] = memory_dump_name
+                item['Plugin'] = plugins
+            with open(output_file_path, 'w') as file:
+                json.dump(data,file)
+            with open(output_file_path, 'r', encoding='utf-8') as file:
+                content = file.read()
+            modified_content = content.replace("[{", "{")
+            modified_content = modified_content.replace("}]", "}")
+            modified_content = modified_content.replace("}, {", "}\n{")
+
+            with open(output_file_path, 'w', encoding='utf-8') as file:
+                file.write(modified_content)
+        except:
+            print("plugin probably didn't work")
+
+    else:
+       output_file_path = os.path.join(output_directory, f"{memory_dump_name}_{plugins}.txt")
+       command = f"python3 {args.volatility_dir}vol.py -f {memory_dump} -s {symbol_dir} {plugins}"
+       with open(output_file_path, 'w') as output_file:
+            subprocess.run(command, shell=True, stdout=output_file)
+       with open(output_file_path, 'r') as output_file:
+            print(output_file.read())
 
 
-  
+
 
 
 for memory_dump in memory_dumps:
@@ -120,9 +149,9 @@ for memory_dump in memory_dumps:
     if symbol_path:
         if isinstance(plugins, str):
             print(f"Running plugin on: {memory_dump}")
-            run_volatility_plugin(memory_dump, str(plugins), symbol_path)
+            run_volatility_plugin(memory_dump, str(plugins), symbol_path, args.output_mode)
         elif isinstance(plugins, list):
             for plugin in plugins:
-                run_volatility_plugin(memory_dump, plugin, symbol_path)
+                run_volatility_plugin(memory_dump, plugin, symbol_path, args.output_mode)
     else:
         print(f"No symbol path found for memory dump: {memory_dump}. Skipping...")
